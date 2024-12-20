2cc87253da795aa45ece5de6848b7c28d37b2308
Avyla <eliamavila780@gmail.com>
2024-12-20 07:59:58 -0500

    Primer commit

---
+++ src/main/java/com/univalle/Guarderia_Control/GuarderiaControlApplication.java
@@ -0,0 +1,13 @@
+package com.univalle.Guarderia_Control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuarderiaControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuarderiaControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univalle/Guarderia_Control/GuarderiaControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univalle.Guarderia_Control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuarderiaControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
